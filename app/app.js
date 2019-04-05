3bb10590974db85e0317aed0a1fab062d40f764e
glofishh <gloriaulee@gmail.com>
2019-04-05 12:20:51 -0400

    why isn't my git remove item not working :'(

--- app/app.js
+++ app/app.js
@@ -1,109 +1,111 @@
-var loadLocalStorage = function () {
-	var keys = Object.keys(localStorage)
-	var htmlString = '';
-	for (var i = 0; i < keys.length; i++) {
-		htmlString += `<tr><td>${keys[i]}</td><td>${localStorage[keys[i]]}</tr></tr>`;
-	}
-	$('tbody').html(htmlString)
-};
+
 
 var updateStatusLabel = function(message) {
-	$('#statusLabel').text('Status: ' + message);
+	$('#statusLabel').text(message);
 }
 
  //jQuery document ready initialization stuff
  ////button and form event handlers
  // logic for determining action probably needs to go in the event handler
 $(document).ready(function () {
-	loadLocalStorage();
 
-	$('#btn-create').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
+	var shoppingArr = [];
+	var listObj = JSON.parse(localStorage.getItem('key'));
 
-		if (keyExists) {
-			updateStatusLabel('key already exists, please use update button instead! :D');
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		}else {
-			createEntry(key, value);
-			updateStatusLabel('key created - ' + key);
-		}
 
-		loadLocalStorage();
+	if (localStorage.getItem('key')) { 
+		shoppingArr = listObj;
+	}
+
+
+	$('.btn-create').on('click', function(e) { 
+		shoppingArr.push($('.key').val());
+		localStorage.setItem('key', JSON.stringify(shoppingArr));
+
+		$('.list-container').append("<li>" + shoppingArr.slice(-1) + "</li>");
+		$('.key').val('');
+		updateStatusLabel(' ' + '"' + (shoppingArr.slice(-1)).toString().toUpperCase() + '"' + ' HAS BEEN ADDED TO YOUR LIST')
 	});
 
-	$('#btn-update').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var existingValue = localStorage.getItem(key)
-		var keyExists = existingValue !== null;
 
-		if (value === existingValue) {
-			updateStatusLabel('key not updated - that value already exists silly! xD')
-		} else if (keyExists) {
-			updateEntry(key, value);
-			updateStatusLabel('key updated - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, please use create button instead! :D');
-		}		
+	$('.body').ready(function(e) {
+		if (shoppingArr.length > 0) {
+			for(var i = 0; i < listObj.length; i++) {
+				$('.list-container').append("<li>" + listObj[i] + "</li>");
+			}
+		}
+	});
+
+	//CANNOT GET TO WORK ROAR
+	$('.btn-delete-item').on('click', function(e) {
+		var key = $('.key').val();
+		// var keyExists = localStorage.getItem(key) !== null;
 		
-		loadLocalStorage();		
-	});
 
-	$('#btn-delete').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
-
-		if (keyExists) {
-			removeEntry(key);
-			updateStatusLabel('key removed - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, nothing removed. :|');
+		for(var i = 0; i < listObj.length; i++) {
+			if(key === listObj[i]) {
+				window.localStorage.removeItem('key');	
+				// $('li').remove(); 
+				updateStatusLabel('item "' + key + '" removed');	
+			} else if (key === '') {
+				updateStatusLabel('pick something to delete')
+			} else {
+				updateStatusLabel('can only delete last item');
+			}
 		}
-
-		loadLocalStorage();
 	});	
 
+	// $('.btn-delete-item').on('click', function(e) {
+	// 	var key = $('#key').val();
+	// 	var keyExists = localStorage.getItem(key) !== null;
+
+	// 	if (keyExists) {
+	// 		removeEntry(key);
+	// 		updateStatusLabel('key removed - ' + key);
+	// 	} else if (key === '') {
+	// 		$('li').remove(); 
+	// 		updateStatusLabel('deleted')
+	// 	} else {
+	// 		updateStatusLabel('nothing to delete');
+	// 	}
+	// });	
+
+	function randomizeMessage() {
+  		var shoppingTagline = ['shop till you drop', 'honey we\'re out of milk again', 'it\'s all fun and games until you\'re out of toilet paper', 'the best part of grocery shopping with kids is staying home and starving instead', 'there\'s no way to look cool trying to pull two shopping baskets apart', 'are you an ikea shopping cart because i have no way of predicting what you\'re going to do next', '(don\'t forget the ice cream)', 'shopshopshop'];
+  		var randomSelect = Math.floor(Math.random() * shoppingTagline.length);
+    	return shoppingTagline[randomSelect];
+    }
+
+	$('.btn-delete-all').on('click', function(e){
+		localStorage.clear();
+		$('li').remove();
+		updateStatusLabel(' ' + (randomizeMessage().toUpperCase()));
+	});
 });
-/*
 
-
-
-When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
-
-let form = document.querySelector("form");
-let guestName = form.elements.guest;
-let hatSize = form.elements["hat-size"];
-*/
 
 /*
 PAGE CONTENT STUFF
 */
 //something to update the table every time localStorage changes
 
-//localStorage stuff
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-////create new entry
-//localStorage.setItem(key, value)
-var createEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
+// //localStorage stuff
+// //https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
 
-////Update existing entry
-//localStorage.setItem(key, value)
-var updateEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
+// ////create new entry
+// //localStorage.setItem(key, value)
+// var createEntry = function(key, value) {
+// 	return localStorage.setItem(key, value);
+// }
 
-////delete existing entry
-//localStorage.removeItem(key)
-var removeEntry = function(key) {
-	return localStorage.removeItem(key);
-}
+// ////Update existing entry
+// //localStorage.setItem(key, value)
+// var updateEntry = function(key, value) {
+// 	return localStorage.setItem(key, value);
+// }
+
+// ////delete existing entry
+// //localStorage.removeItem(key)
+// var removeEntry = function(key) {
+// 	return localStorage.removeItem(key);
+// }
